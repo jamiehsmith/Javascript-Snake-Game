3c603ce1798149044b150ea777c9e952d5362315
Jamie Smith <jamiesmith357@gmail.com>
2021-03-28 19:17:38 -0400

    Snake game setup, adding constructor function.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,32 @@
-function setup() {
+let snake;
 
+function setup() {
+    createCanvas(600, 600);
+    snake = new Snake();
 }
 
 function draw() {
-    
+    // Set background to gray
+    background(51);
+    snake.update();
+    snake.show();
+}
+
+function Snake() {
+    this.x = 0;
+    this.y = 0;
+    this.xspeed = 1;
+    this.yspeed = 0;
+
+    this.update = function() {
+        // Increase speed
+        this.x += this.xspeed;
+        this.y += this.yspeed;
+    }
+
+    this.show = function() {
+        // Make a white rectangle
+        fill(255);
+        rect(this.x, this.y, 10, 10);
+    }
 }
